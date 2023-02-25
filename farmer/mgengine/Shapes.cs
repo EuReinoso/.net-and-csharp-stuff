4d4ffa4c4cb93ca7467a65208ff981463c6a60db
EuReinoso <lucas.reinoso.br@gmail.com>
2023-02-25 11:13:04 -0300

    farmer init

---
+++ farmer/mgengine/Shapes.cs
@@ -0,0 +1,44 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace mgengine
+{
+    public class Rectangle2{
+
+        public Vector2 pos {get;set;}
+        public float width {get;set;}
+        public float height {get;set;}
+        public Color color {get;set;}
+        public Texture2D texture {get;set;}
+        
+        public Rectangle2(float x, float y, float width, float height, Color color){
+
+            this.pos = new Vector2(x, y);
+            this.width = width;
+            this.height = height;
+            this.color = color;
+        }
+
+        public void Load(GraphicsDevice graph){
+            this.texture = new Texture2D(graph, 1, 1);
+            this.texture.SetData(new[] {this.color});
+        }
+
+        public void Draw(SpriteBatch spriteBatch){
+            spriteBatch.Draw(this.texture, new Rectangle((int)this.pos.X, (int)this.pos.Y, (int)this.width, (int)this.height), this.color);
+        }
+        public bool CollideRect(Rectangle2 rect){
+            Rectangle rect1 = new Rectangle((int)this.pos.X, (int)this.pos.Y, (int)this.width, (int)this.height);
+            Rectangle rect2 = new Rectangle((int)rect.pos.X, (int)rect.pos.Y, (int)rect.width, (int)rect.height);
+
+            if (rect1.Intersects(rect2)){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    }
+
+}
+
